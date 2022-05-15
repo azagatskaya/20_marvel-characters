23e382aca8ec93662dcd2b63813134882ebf20fd
Alya Zagatskaya <alya.zagacki@gmail.com>
2022-05-15 16:04:26 +0300

    added_rating_&_classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,10 +41,23 @@ const currentPopup = document.querySelector('.popup');
 const popupCloseIcon = document.querySelector('.close-popup');
 popupCloseIcon.addEventListener('click', popupClose);
 
+function getHeroById(id) {
+	let res = null;
+	if (heroesSet.size > 0) {
+		heroesSet.forEach(hero => {
+			if (hero.id == id) {
+				res = hero;
+			}
+		});
+	}
+	return res;
+}
+
 function popupOpen(e) {
-	renderPopup(e);
+	getHeroById(e.target.id).renderPopup(e);
 	currentPopup.classList.add('open');
 	currentPopup.addEventListener('click', outOfPopupClick);
+	checkRatingInLs(e);
 }
 
 function outOfPopupClick(e) {
@@ -56,38 +69,17 @@ function outOfPopupClick(e) {
 
 function popupClose(e) {
 	document.querySelector('.popup').classList.remove('open');
+	emptyRating();
 	e.preventDefault();
 }
 
 function renderSlides() {
 	const slider = document.querySelector('.image-slider__wrapper');
 	heroes.forEach(el => {
-		let slide = `
-				<div class="image-slider__slide swiper-slide">
-					<div class="image-slider__image">
-						<img class="popup-link" id="${el.id}" src="${el.image}" alt="${el.id}">
-					</div>
-				</div>`;
-		slider.innerHTML += slide;
+		let newHero = new Hero(el);
+		slider.innerHTML += newHero.render();
 	});
 	addSlideListeners();
-}
-
-// <div class="image-slider__hero-name">${el.name}</div>
-
-function renderPopup(e) {
-	heroes.forEach(el => {
-		if (el.id === e.target.id) {
-			document.querySelector('.popup__title').innerHTML = el.name;
-			document.querySelector('.popup__img img').src = el.image;
-			document.querySelector('#universe').innerHTML = el.universe;
-			document.querySelector('#alterego').innerHTML = el.alterego;
-			document.querySelector('#occupation').innerHTML = el.occupation;
-			document.querySelector('#friends').innerHTML = el.friends;
-			document.querySelector('#superpowers').innerHTML = el.superpowers;
-			document.querySelector('#details').innerHTML = el.details;
-		}
-	});
 }
 
 function addSlideListeners() {
@@ -96,6 +88,70 @@ function addSlideListeners() {
 		popupLinks.forEach(el => {
 			el.addEventListener('click', popupOpen);
 		});
-		// 	e.preventDefault();
+	}
+}
+
+// rating
+
+const stars = document.querySelectorAll('.rating__star');
+const rating = document.querySelector('.popup__rating');
+
+function checkRatingInLs(e) {
+	if (isRatingInLS(e.target.id)) {
+		setRating(localStorage.getItem(e.target.id));
+		addRatingListeners();
+	} else {}
+}
+
+function isRatingInLS(id) {
+	return localStorage.getItem(id) ? true : false;
+}
+
+function addRatingListeners() {
+	stars.forEach(star => {
+		star.addEventListener('mouseover', handleStarMouseover);
+		star.addEventListener('click', handleStarClick);
+	});
+}
+
+function handleStarMouseover(e) {
+	setStars(e.target.id);
+	rating.addEventListener('mouseout', handleRatingMouseout);
+}
+
+function setStars(starId) {
+	for (let i = 0; i <= 5; i++) {
+		if (i <= starId) {
+			stars[i].innerHTML = 'star';
+		} else {
+			stars[i].innerHTML = 'grade';
+		}
+	}
+}
+
+function handleStarClick(e) {
+	localStorage.setItem(rating.id, e.target.id);
+	setRating(e.target.id);
+}
+
+function setRating(starId) {
+	setStars(starId);
+	rating.removeEventListener('mouseout', handleRatingMouseout);
+}
+
+function emptyRating() {
+	stars.forEach(el => {
+		el.innerHTML = 'grade';
+	});
+}
+
+function handleRatingMouseout(e) {
+	const popupContent = document.querySelector('.popup__content');
+	if (e.relatedTarget === popupContent) {
+		if (isRatingInLS(rating.id)) {
+			setRating(localStorage.getItem(rating.id));
+		} else {
+			emptyRating();
+		}
 	}
 }
